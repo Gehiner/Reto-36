7129f6b61a1a32a16a34ab64f2c242c6ad12828d
Gehiner <gehinerferley@gmail.com>
2021-12-01 14:42:32 -0500

    Ya funciona todo

--- Funciones.js
+++ Funciones.js
@@ -8,16 +8,22 @@ function aleatorio(min, maxi)
 function jugar()
 {
   var iteracion = 0;
-  for(var x in players)
+  for(var x of players)
   {
+    x.puntaje = 0;
+    console.log(iteracion, 'inicio');
     imagenesDeDados[iteracion].innerHTML = '';
     var puntajeAleatorio ;
-    for(var y = 0; y < 2; y++){
+    for(let y = 0; y < 2; y++){
       puntajeAleatorio = aleatorio(1,6);
       x.puntaje = x.puntaje + puntajeAleatorio;
-      imagenesDeDados[iteracion].innerHTML = imagenesDeDados[iteracion].innerHTML + dados[puntajeAleatorio - 1].imagen.src;
+      imagenesDeDados[iteracion].innerHTML = imagenesDeDados[iteracion].innerHTML + "<img src=" + " \" " + dados[puntajeAleatorio - 1].imagen.src+ " \" " + "/>";
     }
+    totalDePuntos[iteracion].innerHTML = "Total: " +  x.puntaje;
+    iteracion ++;
+    console.log(iteracion, 'final');
   }
+  jugadorGanador();
 }
 
 function cambiarNombreDeJugadores(){
@@ -31,9 +37,37 @@ function cambiarNombreDeJugadores(){
 
 
 function agregarJugadores(){
-  players.push(new Jugadores(playersInGame[0].innerHTML, NaN));
-  players.push(new Jugadores(playersInGame[1].innerHTML, NaN));
-  players.push(new Jugadores(playersInGame[2].innerHTML, NaN));
+  players.push(new Jugadores(playersInGame[0].innerHTML, 0));
+  players.push(new Jugadores(playersInGame[1].innerHTML, 0));
+  players.push(new Jugadores(playersInGame[2].innerHTML, 0));
+  return players;
 }
-imagenesDeDados[0] = 
-jugar();
+
+function jugadorGanador(){
+  var numeroMAyor = players[0].puntaje;
+  for(var y of players)
+  {
+    if(y.puntaje > numeroMAyor){
+      numeroMAyor = y.puntaje;
+    }
+    console.log(y, numeroMAyor, "sacando el numeroMayor");
+  }
+  let iterando = 0;
+  for(let z of players)
+  {
+    if(z.puntaje == numeroMAyor)
+    {
+      playersInGame[iterando].style.setProperty("background-color", "#1CD948");
+      console.log('gano', z)
+    }
+    else{
+      playersInGame[iterando].style.setProperty("background-color", "white");
+      console.log('perdio', z)
+    }
+    iterando++;
+  }
+  console.log(numeroMAyor);
+
+}
+
+
